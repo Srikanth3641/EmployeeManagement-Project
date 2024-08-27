a837b694c02006e044b6cd3d83688fa667344472
Srikanth3641 <srikanthnk3641@gmail.com>
2024-08-27 23:21:49 +0530

    my sping boot project

---
+++ springboot-workspace/backend-springboot1/src/main/java/com/ihub/www/BackendSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.ihub.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringboot1Application.class, args);
+	}
+
+}
